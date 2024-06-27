b0ded87ee462f00b11dabb1068f9afdef5875709
Davide Giliberti <gilo.davide@hotmail.it>
2024-06-27 18:55:19 +0200

    Update AddComment.jsx

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Button, Form, FormGroup } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 class AddComment extends Component {
   state = {
